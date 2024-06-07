1d53b6b85d16240673ad31a83ac70a8877841498
Daniel Vieira <danielviei@hotmail.com>
2024-06-06 22:26:58 -0400

    feat: added pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import {createApp} from 'vue';
+import {createPinia} from 'pinia';
 import App from './App.vue';
 import './assets/css/style.css';
 import router from './router';
 
+const pinia = createPinia ();
 
-createApp (App).use (router).mount ('#app');
+createApp (App).use (router).use (pinia).mount ('#app');
