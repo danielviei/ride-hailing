9708ea5dd52329677739c9381bf884703e765585
Daniel Vieira <danielviei@hotmail.com>
2024-06-06 20:18:51 -0400

    feat: added login view components

---
+++ frontend/src/components/PasswordField.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mb-4">
+    <label :for="id" class="block text-gray-700 text-sm font-bold mb-2">{{
+      label
+    }}</label>
+    <input
+      :id="id"
+      :value="value"
+      @input="updateValue($event.target.value)"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      type="password"
+      placeholder="Enter password"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    updateValue(value) {
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
