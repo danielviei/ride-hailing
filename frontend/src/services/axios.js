a6f499f5e7dd9ae20d43830fdfeba086fb7469d7
Daniel Vieira <danielviei@hotmail.com>
2024-06-08 16:41:27 -0400

    feat: refresh token when it expires

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,8 +1,36 @@
 import axios from 'axios';
+import {useUserStore} from '@/stores/userStore';
+
 
 const api = axios.create ({
   baseURL: process.env.VUE_APP_API_URL,
   timeout: 10000,
 });
+
+// WHEN RECEIVING 401 RESPONSE REFRESH TOKEN
+api.interceptors.response.use (
+  response => {
+    return response;
+  },
+  async error => {
+    const originalRequest = error.config;
+    const userStore = useUserStore ();
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const resp = await api.post ('/auth/refresh', {
+          refreshToken: userStore.refreshToken,
+        });
+        userStore.setToken (resp.data.accessToken);
+        originalRequest.headers.Authorization = `Bearer ${resp.data.accessToken}`;
+        return api (originalRequest);
+      } catch (error) {
+        userStore.logout ();
+      }
+    }
+    return Promise.reject (error);
+  }
+);
 
 export default api;
