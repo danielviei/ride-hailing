acc22585548ea1cc8eafabb316e13c333d11b1d9
Daniel Vieira <danielviei@hotmail.com>
2024-06-09 14:25:31 -0400

    feat: allow only years between 1900 and 2100

--- backend/schemas/cars.js
+++ backend/schemas/cars.js
@@ -1,11 +1,16 @@
 import {z} from 'zod';
 
 const CarSchema = z.object ({
-  brand: z.string ().min(1),
-  model: z.string ().min(1),
-  year: z.number ().int ().refine (year => /^\d{4}$/.test (year), {
-    message: 'El año debe ser un número de 4 dígitos',
-  }),
+  brand: z.string ().min (1),
+  model: z.string ().min (1),
+  year: z
+    .number ()
+    .int ()
+    .min (1900)
+    .max (2100)
+    .refine (year => /^\d{4}$/.test (year), {
+      message: 'El año debe ser un número de 4 dígitos',
+    }),
   status: z.enum (['disponible', 'en servicio', 'en mantenimiento']),
 });
 
